ad3985449f838a44933a160ecfa9c58011fe9350
Lylyy <jory.lydia@gmail.com>
2015-06-09 12:05:53 +0200

    Loader Fonctionnel & Amelioré

--- Assets/Scenes/Activable.cs
+++ Assets/Scenes/Activable.cs
@@ -6,30 +6,14 @@ public class Activable : MonoBehaviour {
 
 	// Toutes les methodes sont en statique pour nous permetre d'instancer de nouveaux objects (ce que MonoBehavour ne permet pas)
 	public GUI GUI;
-	public static float loadingTime = 5; // Temps du Loader
-	public static float startTime = 0; // Temps écoulé depuis le jeu lancé
+	public static float loadingTime = 5f; // Temps du Loader
 	public static string picto;
 
 
-	public static void start () {
-
-		startTime = Time.time;
-
-	}
-
-	public static bool isTimeStarted() {
-		return startTime > 0;
-
-	}
-	
 	public static bool isReady () {
 		return (Time.deltaTime + loadingTime) < Time.time;
 	}
-	
-	public static void reset() {
-		startTime = 0;
 
-	}
 
 	public static bool isActivable (string tag) {
 
--- Assets/Scenes/HandRay.cs
+++ Assets/Scenes/HandRay.cs
@@ -2,64 +2,81 @@
 using System.Collections;
 
 public class HandRay : MonoBehaviour {
-
+	
 	public MouvementPerso mouvementPerso;
+	public HandRay handRay;
 	public CustomGUI customGUI;
 	public Transform curseurRayon;
-
+	
 	public float activeDistance = 10f;
 	public bool showPicto = false;
 	public bool loading = false;
+	public static float loadingTime = 5f; // Temps du Loader
+	public static bool isTimingforObjet = false; // Timer du loading des Objets : le décompte par défaut n'a pas encore commencé.
+	public static float timerObjet = 0; // Loader de chaque GUI. Mémarre tout naturellement à zero.
 	public string pictoMsg;
-
+	
 	void OnGUI () {
+		
 		if (loading) {
 			GUI.Box (new Rect (10,10,100,90), "Loading...");
 		}
-
+		
 		if (showPicto) {
 			GUI.Box (new Rect (10,10,100,90), pictoMsg);
 		}
 	}
-
+	
 	void Update () {
-
+		
+		// Timer du Loader
+		
+		if (isTimingforObjet) {
+			
+			timerObjet = timerObjet + Time.deltaTime;
+			
+		} else {
+			
+			timerObjet=0;
+		}
+		
+		
 		RaycastHit hit;
-
+		
 		// Définition de la longueur du rayonCurseur
 		curseurRayon.localScale = new Vector3 (curseurRayon.localScale.x, activeDistance * 2, curseurRayon.localScale.z);
-
+		
 		// L'axe des Z mis dans la variable profondeur
 		Vector3 profondeur = transform.forward;
-
+		
 		//Met à jour la position de curseurRayon à chaque frame
 		curseurRayon.position = transform.position;
-
+		
 		curseurRayon.up = profondeur;
 		
 		if (Physics.Raycast (transform.position, profondeur, out hit,activeDistance)) {
-
-			if ( Activable.isActivable(hit.transform.tag) ) {
-
 			
-
-				if (!Activable.isTimeStarted()) {
-					Activable.start();
-				}
-
-				if (Activable.isReady()) {
+			if ( Activable.isActivable(hit.transform.tag) && hit.collider.gameObject.CompareTag("Bouche_Aeration") ) {
+				
+				isTimingforObjet = true;
+				
+				if (timerObjet > loadingTime) {
 					pictoMsg = Activable.picto;
 					loading = false;
 					showPicto = true;
-				} else {
+				}  else {
 					loading = true;
 					showPicto = false;
+					
 				}
-			} else {
-				loading = false;
-				showPicto = false;
-				Activable.reset ();
+			}
+				
+				else {
+					isTimingforObjet = false;
+					loading = false;
+					showPicto = false;	
+				}
+				
 			}
 		}
 	}
-}
